13164907efb402b798a49f2449319e0610393643
Mobashir Ahmad <mobashir.ahmad@ness.com>
2018-11-27 16:06:18 +0530

    First Working App

--- todo/src/app/model.ts
+++ todo/src/app/model.ts
@@ -1,7 +1,22 @@
-var model = {
-    user: "Mobashir",
-    item: [{ action: "Buy Flowers", done: false },
-    { action: "Get Shoes", done: false },
-    { action: "Collect Tickets", done: true },
-    { action: "Call Joe", done: false }]
-};
+export class Model {
+    user;
+    item;
+    constructor() {
+        this.user = "Mobashir";
+        this.item = [new ToDoItems("Buy Flowers", false),
+        new ToDoItems("Get Shoes", false),
+        new ToDoItems("Collect Ticket", false),
+        new ToDoItems("Call Yadu", false)]
+
+    }
+}
+export class ToDoItems {
+    action;
+    done;
+    constructor(action, done) {
+        this.action = action;
+        this.done = done;
+    }
+
+}
+
