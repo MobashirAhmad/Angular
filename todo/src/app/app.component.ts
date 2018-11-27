ec8ce8a1d6b60847eb994d36ff511f55470e14d4
Mobashir Ahmad <mobashir.ahmad@ness.com>
2018-11-27 20:08:59 +0530

    Adding To Do Items to Model

--- todo/src/app/app.component.ts
+++ todo/src/app/app.component.ts
@@ -1,18 +1,23 @@
 import { Component } from '@angular/core';
-import { Model } from './model'
+import { Model, ToDoItems } from './model'
+import { ThrowStmt } from '@angular/compiler';
 @Component({
   selector: 'todo-app',
   templateUrl: './app.component.html'
- })
+})
 export class AppComponent {
   model = new Model();
   getName() {
     return this.model.user;
   }
-  getToDoItems()
-  {
+  getToDoItems() {
     //return this.model.item.filter(function (item) { return !item.done });
     //Shortcut using Lambda Expression
-     return this.model.item.filter(item=>!item.done);
+    return this.model.item.filter(item => !item.done);
+  }
+  addItem(newItem) {
+    if (newItem != "") {
+      this.model.item.push(new ToDoItems(newItem, false));
+    }
   }
 }
