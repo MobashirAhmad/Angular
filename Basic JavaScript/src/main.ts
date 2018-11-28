47d98c6253b041d5f5bca915cd8a550274e7c5f9
Mobashir Ahmad <mobashir.ahmad@ness.com>
2018-11-28 20:15:12 +0530

    diff between function expression vs function declartaion

--- Basic JavaScript/src/main.ts
+++ Basic JavaScript/src/main.ts
@@ -1,4 +1,27 @@
 //defining function using function expression
-let myfunc=function(){
-console.log("This is a statement");};
+let myfunc = function () {
+    console.log("This is a statement");
+};
 myfunc();
+
+
+//2nd Way using function declaration
+// function myFunc() {
+//     console.log("This is a statement");
+// }
+// myfunc();
+
+// This works because the browser finds the function declaration when it parses the JavaScript
+// file and sets up the function before the remaining statements are executed, a process known as
+// function hoisting.
+myFunc();
+function myFunc() {
+    console.log("This is a statement");
+}
+
+// Function expressions, however, are not subject to hoisting, which means that this
+// code will not work:
+// myFunc();
+// let myFunc = function() {
+// console.log("This is a statement");
+// };
